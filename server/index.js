86af3f36d0f86945400dbc20924abdc356cc1a24
siddharth <siddharthgusain26011999@gmail.com>
2021-06-16 22:54:12 +0530

    added login and register user api

--- server/index.js
+++ server/index.js
@@ -4,9 +4,23 @@ const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 const helmet = require("helmet");
 const morgan = require("morgan");
+const userRoute = require("./routes/users");
+const authRoute = require("./routes/auth");
 
 dotenv.config();
 
-app.listen(8800,() =>{
+mongoose.connect(process.env.MONGO_URL,
+    {useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true} ,() =>{
+    console.log("connected to mongo");
+});
+
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("common"));
+
+app.use("/api/users" , userRoute);
+app.use("/api/auth" , authRoute);
+
+app.listen(8080,() =>{
     console.log("test");
 });
---
+++ server/routes/users.js
@@ -0,0 +1,6 @@
+const router = require("express").Router();
+
+// update user 
+router.put("/:id" )
+
+module.exports = router;
