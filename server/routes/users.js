b9f74db49e03ebfacae9882b7970de852fbfe386
siddharth <siddharthgusain26011999@gmail.com>
2021-06-17 00:57:17 +0530

    removed follwers bug

--- server/routes/users.js
+++ server/routes/users.js
@@ -35,7 +35,7 @@ router.put("/:id" , async (req,res) =>{
 } );
 
 
-//deleter the user
+//delete the user
 router.delete("/:id" , async (req,res) =>{
 
     if(req.body.userId == req.params.id || req.body.isAdmin){
@@ -63,6 +63,34 @@ router.get("/:id" ,  async (req,res) =>{
         res.status(200).json(other);
     }catch(err){
         res.status(500).json(err);
+    }
+});
+
+
+// follow a user
+router.put("/:id/follow",async (req,res) => {
+    if(req.body.userId !== req.params.id){
+        try{
+
+            const user = await User.findById(req.params.id);
+            const currentUser = await User.findById(req.body.userId);
+            
+            if(!user.followers.includes(req.body.userId)){
+                await user.updateOne({$push : { followers: req.body.userId }});
+                await currentUser.updateOne({$push : { followings: req.body.userId }});
+                res.status(200).json("User has been followed");
+            }
+            else{  
+                res.status(403).json("You already followed this user");
+            }
+
+        }
+        catch(err){
+            res.status(500).json(err);
+        }
+    }
+    else{
+        res.status(403).json("you cant follow urself");
     }
 });
 
