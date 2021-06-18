30a09ecd844f986589a8fd990f6faf5b2a98eb91
siddharth <siddharthgusain26011999@gmail.com>
2021-06-18 19:10:01 +0530

    changes

--- client/src/components/rightbar/Rightbar.jsx
+++ client/src/components/rightbar/Rightbar.jsx
@@ -3,7 +3,74 @@ import './rightbar.css';
 export default function Rightbar() {
     return (
         <div className="rightbar">
-            right bar
+            <div className="rightbarWrapper">
+                <div className="birthdayContainer">
+                    <img className="birthdayImg" src="/assets/gift.png" alt="" />
+                    <span className="birthdayText">
+                        <b>Robert</b> and <b>3 other friends</b> have a birthday today
+                        </span>
+                </div>
+               
+                <img className="rightbarAd" src="/assets/ad.jpg" alt="" />
+                <h4 className="rightbarTitle">Online Friends</h4>
+                <ul className="rightbarFriendList">
+                    <li className="rightbarFriend">
+                        <div className="rightbarProfileImgContainer">
+                            <img 
+                             className="rightbarProfileImg"
+                             src="/assets/person/3.jpeg"
+                             alt=""
+                             />
+                             <span className="rightbarOnline"></span>
+                        </div>
+                        <span className="rightbarUsername">Iron man</span>
+                    </li>
+                    <li className="rightbarFriend">
+                        <div className="rightbarProfileImgContainer">
+                            <img 
+                             className="rightbarProfileImg"
+                             src="/assets/person/3.jpeg"
+                             alt=""
+                             />
+                             <span className="rightbarOnline"></span>
+                        </div>
+                        <span className="rightbarUsername">Iron man</span>
+                    </li>
+                    <li className="rightbarFriend">
+                        <div className="rightbarProfileImgContainer">
+                            <img 
+                             className="rightbarProfileImg"
+                             src="/assets/person/3.jpeg"
+                             alt=""
+                             />
+                             <span className="rightbarOnline"></span>
+                        </div>
+                        <span className="rightbarUsername">Iron man</span>
+                    </li>
+                    <li className="rightbarFriend">
+                        <div className="rightbarProfileImgContainer">
+                            <img 
+                             className="rightbarProfileImg"
+                             src="/assets/person/3.jpeg"
+                             alt=""
+                             />
+                             <span className="rightbarOnline"></span>
+                        </div>
+                        <span className="rightbarUsername">Iron man</span>
+                    </li>
+                    <li className="rightbarFriend">
+                        <div className="rightbarProfileImgContainer">
+                            <img 
+                             className="rightbarProfileImg"
+                             src="/assets/person/3.jpeg"
+                             alt=""
+                             />
+                             <span className="rightbarOnline"></span>
+                        </div>
+                        <span className="rightbarUsername">Iron man</span>
+                    </li>
+                </ul>
+            </div>
         </div>
     )
 }
