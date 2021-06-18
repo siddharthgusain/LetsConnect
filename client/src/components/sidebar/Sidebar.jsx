3c39d118f38632aea73ab753c9a10a02ede2cfbb
siddharth <siddharthgusain26011999@gmail.com>
2021-06-19 02:13:33 +0530

    added profile page

--- client/src/components/sidebar/Sidebar.jsx
+++ client/src/components/sidebar/Sidebar.jsx
@@ -16,41 +16,41 @@ export default function Sidebar(){
                 <ul className="sidebarList">
 
                     <li className="sidebarListItem">
-                        <RssFeed className="sidebarIcon"/>
+                        <RssFeed htmlColor = "orange" className="sidebarIcon"/>
                         <span className="sidebarListItemText">Feed</span>
                     </li>
 
                     <li className="sidebarListItem">
-                        <Chat className="sidebarIcon" />
+                        <Chat htmlColor = "green" className="sidebarIcon" />
                         <span className="sidebarListItemText">Chats</span>
                     </li>
 
                     <li className="sidebarListItem">
-                        <PlayCircleFilledOutlined className="sidebarIcon" />
+                        <PlayCircleFilledOutlined htmlColor="blue" className="sidebarIcon" />
                         <span className="sidebarListItemText">Videos</span>
                     </li>
 
                     <li className="sidebarListItem">
-                        <Group className="sidebarIcon" />
+                        <Group htmlColor = "pink" className="sidebarIcon" />
                         <span className="sidebarListItemText">Groups</span>
                     </li>
 
                     <li className="sidebarListItem">
-                        <Bookmark className="sidebarIcon" />
+                        <Bookmark htmlColor="yellow" className="sidebarIcon" />
                         <span className="sidebarListItemText">Bookmarks</span>
                     </li>
                     <li className="sidebarListItem">
-                        <HelpOutline className="sidebarIcon" />
+                        <HelpOutline htmlColor="#BEFF33" className="sidebarIcon" />
                         <span className="sidebarListItemText">Questions</span>
                     </li>
 
                     <li className="sidebarListItem">
-                        <WorkOutline className="sidebarIcon" />
+                        <WorkOutline htmlColor="#33FCFF" className="sidebarIcon" />
                         <span className="sidebarListItemText">Jobs</span>
                     </li>
 
                     <li className="sidebarListItem">
-                        <Event className="sidebarIcon" />
+                        <Event htmlColor="#5533FF" className="sidebarIcon" />
                         <span className="sidebarListItemText">Events</span>
                     </li>
 
