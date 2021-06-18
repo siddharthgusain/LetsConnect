37b818fe032661763ed41ef0ec0759c57ba881fd
siddharth <siddharthgusain26011999@gmail.com>
2021-06-18 15:45:35 +0530

    added some components

---
+++ client/src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,12 @@
+import './sidebar.css';
+
+export default function Sidebar(){
+    return (
+        <div className="sidebar">
+            <div className="sidebarWrapper">
+                wrap
+                
+            </div>
+        </div>
+    )
+}  
