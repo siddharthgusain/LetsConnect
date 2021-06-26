e66d799e46b80e2c7d1dec12da1a2b74c3ab30b4
siddharth <siddharthgusain26011999@gmail.com>
2021-06-26 18:03:11 +0530

    added image upload

--- client/src/components/share/Share.jsx
+++ client/src/components/share/Share.jsx
@@ -1,30 +1,66 @@
 import './share.css';
 import { PermMedia ,Label , Room , EmojiEmotions } from "@material-ui/icons";
-import { useContext } from 'react';
+import { useContext, useRef, useState } from 'react';
 import { AuthContext } from '../../context/AuthContext';
+import axios from 'axios';
 
 export default function Share() {
 
     const { user } = useContext(AuthContext);
     
     const PF = process.env.REACT_APP_PUBLIC_FOLDER;
+
+    const desc = useRef();
+    const [file , setFile] = useState(null);
+
+    const submitHandler = async (e) =>{
+
+        e.preventDefault();
+        const  newPost = {
+            userId : user._id,
+            desc : desc.current.value
+        }
+
+        try{
+
+            await axios.post("/posts",newPost);
+        }catch(err){
+            
+        }
+    }
+
     return (
+
         <div className="share">
             <div className="shareWrapper">
                 <div className="shareTop">
-                    <img className="shareProfileImg" src={PF + user.profilePicture } alt="" />
+                    <img className="shareProfileImg"
+                         src={
+                             user.profilePicture ? PF + user.profilePicture
+                             : PF + "person/noAvatar.png" 
+                            } 
+                         alt="" />
                     <input 
-                        placeholder="Share What's on your mind siddharth"
+                        placeholder={ "Share what's on your mind " + user.username + " ?"}
                         className ="shareInput"
+                        ref = { desc } 
                     />
                 </div>
                 <hr className="shareHr" />
-                <div className="shareButtom">
+                <form className="shareButtom" onSubmit = { submitHandler }>
                     <div className="shareOptions" >
-                        <div className="shareOption">
+                        <label htmlFor = "file" className="shareOption">
+                             
                             <PermMedia htmlColor = "tomato" className="shareIcon"/>
                             <span className="shareOptionText">Photo/Video</span>
-                        </div>
+                            <input
+                                 style = { { display : "none" } }
+                                 type = "file" id = "file" 
+                                 accept = ".png,.jpeg,.jpg" 
+                                 onChange ={(e) => setFile( e.target.files[0])}
+                            />
+                            
+                        </label>
                         <div className="shareOption">
                              <Label  htmlColor = "blue" className="shareIcon"/>
                              <span className="shareOptionText">Tag</span>
@@ -38,8 +74,8 @@ export default function Share() {
                             <span className="shareOptionText">Feelings</span>
                         </div>  
                     </div>
-                    <button className="shareButton">Share</button>
-                </div>
+                    <button className="shareButton" type="submit" >Share</button>
+                </form>
             </div>
         </div>
     )
--- server/index.js
+++ server/index.js
@@ -7,6 +7,7 @@ const morgan = require("morgan");
 const userRoute = require("./routes/users");
 const authRoute = require("./routes/auth");
 const postRoute = require("./routes/posts");
+const multer = require('multer');
 
 dotenv.config();
 
@@ -18,6 +19,26 @@ mongoose.connect(process.env.MONGO_URL,
 app.use(express.json());
 app.use(helmet());
 app.use(morgan("common"));
+
+const storage = multer.diskStorage({
+
+    destination:(req,file , cb) =>{
+        cb(null , "public/images");
+    },
+    filename: (req , file, cb) =>{
+        cb(null,req.body.name);
+    }
+});
+
+const upload  = multer(storage);
+
+app.post("/api/upload", upload.single("file") , (req,res) =>{
+    try{
+        return res.status(200).json("File uploaded successfully.");
+    }catch(err){
+        console.log(err);
+    }
+});
 
 app.use("/api/users" , userRoute);
 app.use("/api/auth" , authRoute);
